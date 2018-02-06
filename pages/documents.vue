dff22925b5e2d7a59ac5690be5c99d42fdfb9c0e
Joshua Swiss <swissjoshua@gmail.com>
2018-02-06 18:20:08 +0000

    simple list filter for docs. needs style and actual data

---
+++ pages/documents.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="documents">
+    <form action="">
+      <input type="text" name="query" placeholder="filter documents" v-model="searchQuery">
+    </form>
+    <ul id="doc-list">
+      <li v-for="link in filteredDocs" :key="link.id"><a href="link.url">{{ link.text }}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+  	name: 'documents',
+  	data() {
+  		return {
+  			links: [
+  				{ id: 1, text: 'Johnny', url: 'https://google.com' },
+  				{ id: 2, text: 'Joey', url: 'https://google.com' },
+  				{ id: 3, text: 'Jimmy', url: 'https://google.com' },
+  				{ id: 4, text: 'Turducken', url: 'https://google.com' },
+  			],
+  			searchQuery: '',
+  		};
+  	},
+  	computed: {
+  		filteredDocs() {
+  			return this.links.filter(link => {
+  				return link.text.toLowerCase().includes(this.searchQuery.toLowerCase());
+  			});
+  		},
+  	},
+  };
+</script>
+
+<style scoped>
+  #documents {
+  	margin-top: 5%;
+  }
+</style>
+
