d25101eada9301e9f28addab68f9ac8adbdcbe76
Joshua Swiss <swissjoshua@gmail.com>
2018-02-08 14:48:35 +0000

    fetching table data to index for now

---
+++ pages/helpers.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+export async function getPooledData({ store, redirect, client }) {
+  if (client) return;
+
+  try {
+    const res = await axios.get(
+      'https://somalia-aid-flows.herokuapp.com/pooled'
+    );
+    store.commit('getPooledData', res.data);
+  } catch (err) {
+    // store.commit('init', []);
+    // error({ statusCode: 500, message: 'Oops! Try again!' });
+    redirect('/err');
+  }
+}
+
+export async function getProjectData({ store, redirect, client }) {
+  if (client) return;
+
+  try {
+    const res = await axios.get(
+      'https://somalia-aid-flows.herokuapp.com/project-table'
+    );
+    store.commit('getProjectData', res.data);
+  } catch (err) {
+    // store.commit('init', []);
+    // error({ statusCode: 500, message: 'Oops! Try again!' });
+    redirect('/err');
+  }
+}
+
+export async function getEnvelopeData({ store, redirect, client }) {
+  if (client) return;
+
+  try {
+    const res = await axios.get(
+      'https://somalia-aid-flows.herokuapp.com/envelopes'
+    );
+    store.commit('getEnvelopeData', res.data);
+  } catch (err) {
+    // store.commit('init', []);
+    // error({ statusCode: 500, message: 'Oops! Try again!' });
+    // redirect('/err');
+    console.log(err);
+  }
+}
