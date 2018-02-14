370526a22c5862aa3e7f8560065c79fe2f753f09
Joshua Swiss <swissjoshua@gmail.com>
2018-02-14 07:56:38 +0000

    three location tables rendering

--- pages/tables/locations.vue
+++ pages/tables/locations.vue
@@ -1,34 +1,46 @@
 <template>
   <div id="locations" class="container is-fluid">
     <table-tabs></table-tabs>
-      <div class="field-body">
-        <div class="field is-narrow">
-          <div class="control">
-            <label class="radio">
-              <input type="radio" value="2016" name="2016" @click="toggle2016">
-              XAF
-            </label>
-            <label class="radio">
-              <input type="radio" value="2017" name="2017" @click="toggle2017">
-              EUR
-            </label>
-            <label class="radio">
-              <input type="radio" value="2018" name="2018" @click="toggle2018">
-              USD
-            </label>
-          </div>
-        </div>
+    <div id="table" class="columns">
+      <div class="column is-4 buffer">
+        <h1 class="title">2016</h1>
+        <h2 class="subtitle">Count and Value of Projects by Location</h2>
+        <no-ssr placeholder="Loading...">
+          <v-client-table
+            name="location-table-2016"
+            :data="location2016Table"
+            :columns="columns"
+            :options="options"
+          >
+          </v-client-table>
+        </no-ssr>
       </div>
-    <div id="table">
-      <no-ssr placeholder="Loading...">
-        <v-client-table
-          :data="locationTable"
-          :columns="columns"
-          :options="options"
-        >
-
-      </v-client-table>
-      </no-ssr>
+      <div class="column is-4 buffer">
+        <h1 class="title">2017</h1>
+        <h2 class="subtitle">Count and Value of Projects by Location</h2>
+        <no-ssr>
+          <v-client-table
+            name="location-table-2017"
+            :data="location2017Table"
+            :columns="columns"
+            :options="options"
+          >
+          </v-client-table>
+        </no-ssr>
+      </div>
+      <div class="column is-4 buffer">
+        <h1 class="title">2018</h1>
+        <h2 class="subtitle">Count and Value of Projects by Location</h2>
+        <no-ssr>
+          <v-client-table
+            name="location-table-2018"
+            :data="location2018Table"
+            :columns="columns"
+            :options="options"
+          >
+          </v-client-table>
+        </no-ssr>
+      </div>
     </div>
   </div>
 </template>
@@ -36,9 +48,7 @@
 <script>
   import { mapState, mapMutations, mapActions } from 'vuex';
   import axios from 'axios';
-  import { getLocationData } from '../helpers';
   import TableTabs from '~/components/TableTabs';
-  // import VClientTable from 'vue-bulma-tables-2';
 
   export default {
   	name: 'locations',
@@ -48,16 +58,18 @@
   				saveState: true,
   				storage: 'local',
   				highlightMatches: true,
+  				pagination: false,
   			},
   		};
   	},
   	components: {
   		TableTabs,
   	},
-  	fetch: getLocationData,
   	computed: {
   		...mapState({
-  			locationTable: state => state.locationsTable,
+  			location2016Table: state => state.location2016Table,
+  			location2017Table: state => state.location2017Table,
+  			location2018Table: state => state.location2018Table,
   			columns: state => state.locationsTableColumns,
   		}),
   	},
