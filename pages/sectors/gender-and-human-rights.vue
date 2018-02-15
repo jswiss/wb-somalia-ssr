4d425534e28d25d0504051a75974a96c3b4d93bf
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-15 10:39:21 +0000

    gender and human rights charts showing

--- pages/sectors/gender-and-human-rights.vue
+++ pages/sectors/gender-and-human-rights.vue
@@ -2,6 +2,8 @@
   <div id="sectors" class="container is-fluid">
     Gender and Human Rights
     <div id="container"></div>
+    <div id="stack-chart"></div>
+    <div id="pie-chart">Figure 32, wrong Pie is shown</div>
   </div>
 </template>
 
@@ -16,6 +18,11 @@ export default {
         ["Mix of implementers - involving government", 3700000],
         ["NGOs", 31539718],
         ["Private Sector", 695274]
+      ],
+      genderHumanRightsDataChart: [
+        ["2016", 16.0],
+        ["2017", 13.0],
+        ["2018", 7.1]
       ],
     };
   },
@@ -60,10 +67,53 @@ export default {
       chart.container('container');
 
       chart.draw();
+    },
+    renderYearChart() {
+      const chart = anychart.cartesian();
+      const dataSet = anychart.data.set(this.genderHumanRightsDataChart);
+
+      const seriesData = dataSet.mapAs({x: 0, value: 1});
+
+      const column = chart.column(seriesData);
+      column
+      .labels()
+      .enabled(true)
+      .format('${%Value}');
+
+      chart.animation(true);
+
+      chart.title("Gender & Human Rights: Project Disbursements ");
+
+      chart.yScale().minimum(0);
+
+      chart
+      .tooltip()
+      .displayMode('union')
+      .position('point')
+      .unionFormat(function(){
+        return `Plain: $${
+          this.points[0].value
+        } mln \n Fact: $${this.points[0].value} `;
+      });
+
+      chart.interactivity().hoverMode('by-x');
+
+      chart
+      .yAxis()
+      .labels()
+      .format('${%Value} mln');
+
+      chart.xAxis(true);
+
+      chart.container('stack-chart');
+
+      chart.draw();
     }
+
   },
   mounted() {
     this.renderChart();
+    this.renderYearChart();
   }
 };
 </script>
@@ -77,4 +127,5 @@ export default {
   margin: 0;
   padding: 0;
 }
+
 </style>
