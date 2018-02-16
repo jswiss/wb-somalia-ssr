83c8d50b1c5a1f1c0c14b520b154aa3f8a6bdf37
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-16 16:06:09 +0000

    Hiirshabelle and Jubaland added

--- pages/locations/hiirshabelle.vue
+++ pages/locations/hiirshabelle.vue
@@ -1,18 +1,146 @@
-<template>
-  <div id="hiirshabelle">
-    hiirshabelle
+<template lang="html">
+  <div id="layouts" class="container is-fluid">
+    Hiirshabelle
+    <div id="container"></div>
+    <div id="project-disbursements"></div>
+    <div id="peace-security"></div>
+    <div id="institutions"></div>
+    <div id="economic-growth"></div>
+    <div id="infrastructure"></div>
+    <div id="social-human"></div>
+    <div id="reslience"></div>
   </div>
 </template>
 
 <script>
-  import LocationsMenu from '~/components/LocationsMenu';
-  export default {
-  	layout: 'locations',
-  };
+export default {
+  layout: 'locations',
+  data() {
+    return {
+      hiirshabelleDisbursementClusterData: [
+        ["Peace, Inclusive Politics, Security & Rule of Law", 10, 8.1, 2.6],
+        ["Effective, Efficient Institutions", 2.9, 2.9, 1.8],
+        ["Economic Growth", 6.3, 5.1, 3.6],
+        ["Infrastructure", 1.6, 1, 1.4],
+        ["Social & Human Development", 20.2, 23.6, 9],
+        ["Reslience", 20.5, 40, 13.3]
+      ],
+      hiirshabellePeacePoliticsSecurityClusterData: [
+        ["2016", 6.5, 2.8, 1.1],
+        ["2017", 3.2, 4.4, 0.6],
+        ["2018", 0.3, 1, 0.9]
+      ],
+      hiirshabelleInstititionsClusterData: [
+        ["Civil Service Reform/Public Administration", 0.3, 0.3, 0.2],
+        ["Planning, M & E and Statistics", 0.2, 0.2, 0.1],
+        ["Public Financial Management", 1.1, 0.8, 1.5],
+        ["State and Local Governance", 1.3, 1.5, 0]
+      ],
+      hiirshabelleEconomicClusterData: [
+        ["Private Sector Development", 4.7, 3.6, 3.5],
+        ["Productive Sectors (Agriculture, Fisheries, Livestock)", 1.6, 1.5, 0.1]
+      ],
+      hiirshabelleInfrastructureClusterData: [
+        ["Energy and ICT", 1.4, 0.1, 0],
+        ["Mixed Infrastructure", 0.1, 0.6, 0.8],
+        ["Transport: Roads, Ports, Airports", 0, 0, 0],
+        ["Water and Sanitation (Urban)", 0.1, 0.3, 0.6]
+      ],
+      hiirshabelleSocialHumanDevClusterData: [
+        ["Education", 6.9, 4.7, 0.4],
+        ["Health", 13.2, 16.7, 8.5]
+      ],
+      hiirshabelleResilienceClusterData: [
+        ["Migration, Displayment, Refugees and Durable Solutions", 2.4, 5.7, 0.1],
+        ["Natural Resources Management", 5, 3.2, 0.4],
+        ["Social Protection", 0, 0, 0],
+        ["Food Security", 3.1, 4.4, 7.5],
+        ["Lifesaving humanitarian - cross-cutting", 5.6, 21.5, 3.9]
+      ]
+    };
+  },
+
+  methods: {
+    renderClusterChart(chartData, chartTitle, container) {
+      const chart = anychart.cartesian();
+      const dataSet = anychart.data.set(chartData);
+
+      var seriesData_1 = dataSet.mapAs({x: 0, value: 1});
+      var seriesData_2 = dataSet.mapAs({x: 0, value: 2});
+      var seriesData_3 = dataSet.mapAs({x: 0, value: 3});
+
+      var series1 = chart.column(seriesData_1);
+      var series2 = chart.column(seriesData_2);
+      var series3 = chart.column(seriesData_3);
+
+      // set the padding between columns
+      chart.barsPadding(0);
+
+      // set the padding between column groups
+      chart.barGroupsPadding(2);
+
+      // const column = chart.column(seriesData);
+      // column
+      // .labels()
+      // .enabled(true)
+      // .format('${%Value}');
+
+      chart.animation(true);
+
+      chart.title(chartTitle);
+
+      chart.yScale().minimum(0);
+
+      chart
+      .tooltip()
+      .displayMode('union')
+      .position('point')
+      .unionFormat(function(){
+        return `Plain: $${
+          this.points[0].value
+        } mln \n Fact: $${this.points[0].value} `;
+      });
+
+      chart.interactivity().hoverMode('by-x');
+
+      chart
+      .yAxis()
+      .labels()
+      .format('${%Value} mln');
+
+      chart.xAxis(true);
+
+      chart.container(container);
+
+      chart.draw();
+    },
+  },
+  mounted() {
+    this.renderClusterChart(this.hiirshabelleDisbursementClusterData, "Hiirshabelle: Project Disbursements by Pillar, 2016-18", 'project-disbursements');
+
+    this.renderClusterChart(this.hiirshabellePeacePoliticsSecurityClusterData, "Hiirshabelle: Peace, Inclusive Politics, Security and Rule of Law", 'peace-security');
+
+    this.renderClusterChart(this.hiirshabelleInstititionsClusterData, "Hiirshabelle: Effective, Efficient Institutions", 'institutions');
+
+    this.renderClusterChart(this.hiirshabelleEconomicClusterData, "Hiirshabelle: Economic Growth", 'economic-growth');
+
+    this.renderClusterChart(this.hiirshabelleInfrastructureClusterData, "Hiirshabelle: Infrastructure", 'infrastructure');
+
+    this.renderClusterChart(this.hiirshabelleSocialHumanDevClusterData, "Hiirshabelle: Social & Human Development", 'social-human');
+
+    this.renderClusterChart(this.hiirshabelleResilienceClusterData, "Hiirshabelle: Resilience", 'reslience');
+
+  }
+};
 </script>
 
 <style scoped>
-  #hiirshabelle {
-  	margin-top: 5%;
-  }
+/* make sure div#id has a size defined, otherwise nothing will render */
+
+#container {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 </style>
--- pages/locations/jubaland.vue
+++ pages/locations/jubaland.vue
@@ -1,18 +1,146 @@
-<template>
-  <div id="jubaland">
+<template lang="html">
+  <div id="layouts" class="container is-fluid">
     Jubaland
+    <div id="container"></div>
+    <div id="project-disbursements"></div>
+    <div id="peace-security"></div>
+    <div id="institutions"></div>
+    <div id="economic-growth"></div>
+    <div id="infrastructure"></div>
+    <div id="social-human"></div>
+    <div id="reslience"></div>
   </div>
 </template>
 
 <script>
-  import LocationsMenu from '~/components/LocationsMenu';
-  export default {
-  	layout: 'locations',
-  };
+export default {
+  layout: 'locations',
+  data() {
+    return {
+      jubalandDisbursementClusterData: [
+        ["Peace, Inclusive Politics, Security & Rule of Law", 21.5, 21.1, 7.8],
+        ["Effective, Efficient Institutions", 12.6, 11.7, 5.0],
+        ["Economic Growth", 7.6, 7.7, 2.9],
+        ["Infrastructure", 0.6, 2.7, 2.5],
+        ["Social & Human Development", 15.8, 23.9, 12.7],
+        ["Reslience", 34.2, 65.3, 21.6]
+      ],
+      jubalandPeacePoliticsSecurityClusterData: [
+        ["2016", 15.9, 9.4, 4.6],
+        ["2017", 4.2, 4.4, 0.7],
+        ["2018", 1.4, 7.2, 2.6]
+      ],
+      jubalandInstititionsClusterData: [
+        ["Civil Service Reform/Public Administration", 0.5, 0.7, 0.2],
+        ["Planning, M & E and Statistics", 1.9, 1.8, 1.7],
+        ["Public Financial Management", 2.5, 2.4, 3],
+        ["State and Local Governance", 7.7, 6.8, 0.2]
+      ],
+      jubalandEconomicClusterData: [
+        ["Private Sector Development", 6.7, 4.8, 2.3],
+        ["Productive Sectors (Agriculture, Fisheries, Livestock)", 0.9, 2.9, 0.6]
+      ],
+      jubalandInfrastructureClusterData: [
+        ["Energy and ICT", 0.1, 0.2, 0.2],
+        ["Mixed Infrastructure", 0.3, 1.7, 0.6],
+        ["Transport: Roads, Ports, Airports", 0, 0.3, 0.8],
+        ["Water and Sanitation (Urban)", 0.1, 0.5, 1.0]
+      ],
+      jubalandSocialHumanDevClusterData: [
+        ["Education", 2.0, 6.3, 1.4],
+        ["Health", 13.6, 15.2, 11.3]
+      ],
+      jubalandResilienceClusterData: [
+        ["Migration, Displayment, Refugees and Durable Solutions", 5.2, 15.7, 3.5],
+        ["Natural Resources Management", 5, 3.9, 0.5],
+        ["Social Protection", 5.3, 2.3, 2.3],
+        ["Food Security", 2.7, 11.5, 10.1],
+        ["Lifesaving humanitarian - cross-cutting", 5.6, 21.5, 3.9]
+      ]
+    };
+  },
+
+  methods: {
+    renderClusterChart(chartData, chartTitle, container) {
+      const chart = anychart.cartesian();
+      const dataSet = anychart.data.set(chartData);
+
+      var seriesData_1 = dataSet.mapAs({x: 0, value: 1});
+      var seriesData_2 = dataSet.mapAs({x: 0, value: 2});
+      var seriesData_3 = dataSet.mapAs({x: 0, value: 3});
+
+      var series1 = chart.column(seriesData_1);
+      var series2 = chart.column(seriesData_2);
+      var series3 = chart.column(seriesData_3);
+
+      // set the padding between columns
+      chart.barsPadding(0);
+
+      // set the padding between column groups
+      chart.barGroupsPadding(2);
+
+      // const column = chart.column(seriesData);
+      // column
+      // .labels()
+      // .enabled(true)
+      // .format('${%Value}');
+
+      chart.animation(true);
+
+      chart.title(chartTitle);
+
+      chart.yScale().minimum(0);
+
+      chart
+      .tooltip()
+      .displayMode('union')
+      .position('point')
+      .unionFormat(function(){
+        return `Plain: $${
+          this.points[0].value
+        } mln \n Fact: $${this.points[0].value} `;
+      });
+
+      chart.interactivity().hoverMode('by-x');
+
+      chart
+      .yAxis()
+      .labels()
+      .format('${%Value} mln');
+
+      chart.xAxis(true);
+
+      chart.container(container);
+
+      chart.draw();
+    },
+  },
+  mounted() {
+    this.renderClusterChart(this.jubalandDisbursementClusterData, "Jubaland: Project Disbursements by Pillar, 2016-18", 'project-disbursements');
+
+    this.renderClusterChart(this.jubalandPeacePoliticsSecurityClusterData, "Jubaland: Peace, Inclusive Politics, Security and Rule of Law", 'peace-security');
+
+    this.renderClusterChart(this.jubalandInstititionsClusterData, "Jubaland: Effective, Efficient Institutions", 'institutions');
+
+    this.renderClusterChart(this.jubalandEconomicClusterData, "Jubaland: Economic Growth", 'economic-growth');
+
+    this.renderClusterChart(this.jubalandInfrastructureClusterData, "Jubaland: Infrastructure", 'infrastructure');
+
+    this.renderClusterChart(this.jubalandSocialHumanDevClusterData, "Jubaland: Social & Human Development", 'social-human');
+
+    this.renderClusterChart(this.jubalandResilienceClusterData, "Jubaland: Resilience", 'reslience');
+
+  }
+};
 </script>
 
 <style scoped>
-  #jubaland {
-  	margin-top: 5%;
-  }
+/* make sure div#id has a size defined, otherwise nothing will render */
+
+#container {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 </style>
