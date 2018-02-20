8046b21b1749378e2ed685808ac164487ad96ea3
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-20 17:13:56 +0000

    removed ubunto folder thing

--- ubuntu@ec2-18-219-197-125.us-east-2.compute.amazonaws.com/plugins/anychart.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import anychart from 'anychart';
-
-Vue.use(anychart);
--- ubuntu@ec2-18-219-197-125.us-east-2.compute.amazonaws.com/plugins/vue-bulma-tables-2.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import { ClientTable } from 'vue-bulma-tables-2';
-
-Vue.use(ClientTable, {}, false);
