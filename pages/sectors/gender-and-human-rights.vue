cf85d132f912c76d312dabf5731de0e96382e6c5
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-21 14:18:12 +0000

    g&hr tooltips done

--- pages/sectors/gender-and-human-rights.vue
+++ pages/sectors/gender-and-human-rights.vue
@@ -220,17 +220,23 @@ export default {
       chart
       .column(seriesData_1)
       .color(this.$store.state.color.blue)
-      .name("2018");
+      .name("2018")
+      .tooltip()
+      .format('2018: ${%Value} mln');
 
       chart
       .column(seriesData_2)
       .color(this.$store.state.color.green)
       .name("2017")
+      .tooltip()
+      .format('2017: ${%Value} mln');
 
       chart
       .column(seriesData_3)
       .color(this.$store.state.color.yellow)
-      .name("2016");
+      .name("2016")
+      .tooltip()
+      .format('2016: ${%Value} mln');
 
       // set the chart title
       chart.title("Gender & Human Rights: Project Disbursements by Location");
