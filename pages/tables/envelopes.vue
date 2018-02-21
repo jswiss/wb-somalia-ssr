84edde07d0a4c1fbd1d863a4a648da75166be88e
Joshua Swiss <swissjoshua@gmail.com>
2018-02-21 11:21:17 +0000

    added sparklines to envelopes table

--- pages/tables/envelopes.vue
+++ pages/tables/envelopes.vue
@@ -12,32 +12,37 @@
             >
               <template slot="2016" slot-scope="props">
                 <div>
-                  $ {{ props.row['2016'] | currency }}
+                  ${{ props.row['2016'] | currency }}
                 </div>
               </template>
               <template slot="2017" slot-scope="props">
                 <div>
-                  $ {{ props.row['2017'] | currency }}
+                  ${{ props.row['2017'] | currency }}
                 </div>
               </template>
               <template slot="2018" slot-scope="props">
                 <div>
-                 $  {{ props.row['2018'] | currency }}
+                 ${{ props.row['2018'] | currency }}
                 </div>
               </template>
               <template slot="2019" slot-scope="props">
                 <div>
-                  $ {{ props.row['2019'] | currency }}
+                  ${{ props.row['2019'] | currency }}
                 </div>
               </template>
               <template slot="2020" slot-scope="props">
                 <div>
-                  $ {{ props.row['2020'] | currency }}
+                  ${{ props.row['2020'] | currency }}
                 </div>
               </template>
               <template slot="Total 2016-18" slot-scope="props">
                 <div>
-                  $ {{ props.row['Total 2016-18'] | currency }}
+                  ${{ props.row['Total 2016-18'] | currency }}
+                </div>
+              </template>
+              <template slot="Total 2016-18" slot-scope="props">
+                <div>
+                  <trend :data="[props.row['2016'], props.row['2017'], props.row['2018'], props.row['2019'], props.row['2020'] ]" auto-draw smooth></trend>
                 </div>
               </template>
 
