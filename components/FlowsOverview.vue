985b5355012851c65fb637dfc991b5fb855b88ce
Joshua Swiss <swissjoshua@gmail.com>
2018-02-23 12:50:17 +0000

    overview of flows up

--- components/FlowsOverview.vue
+++ components/FlowsOverview.vue
@@ -2,7 +2,7 @@
   <div id="overview">
     <div class="columns">
       <div class="column is-1"></div>
-      <h1 class="column title">Overview of Flows</h1>
+      <h1 class="column title is-3">Overview of Flows</h1>
     </div>
     <div class="columns">
       <div class="column is-5">
@@ -11,6 +11,77 @@
       <div class="column is-1"></div>
       <div class="column is-5">
         <div id="oda"></div>
+      </div>
+    </div>
+    <div class="columns" id="tables">
+      <div class="column is-1"></div>
+      <div class="column is-5 buffer">
+        <h3 class="title is-4">Reported Humanitarian Aid By Partner</h3>
+        <h6 class="subtitle is-6">US$ Millions, listed in order of magnitude of total aid reported for 2016-18</h6>
+        <table class="table is-bordered is-striped is-narrow is-hoverable">
+          <thead>
+            <tr>
+              <th>Partner</th>
+              <th>2016</th>
+              <th>2017</th>
+              <th>2018</th>
+              <th>Total, 2016-18</th>
+              <th>Trend</th>
+            </tr>
+          </thead>
+          <tbody v-for="row in humanitarianAid" :key="row.partner">
+            <td>{{ row.partner }}</td>
+            <td>{{ row['2016'] }}</td>
+            <td>{{ row['2017'] }}</td>
+            <td>{{ row['2018'] === 0 ? '' : row['2018']  }}</td>
+            <td>{{ row.total }}</td>
+            <td><trend :data="[row['2016'], row['2017'], row['2018']]" auto-draw smooth></trend></td>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>TOTAL</th>
+              <th>563.0</th>
+              <th>1010.7</th>
+              <th>93.3</th>
+              <th>1,666.9</th>
+              <th><trend :data="[563.0, 1010.7, 93.3]" auto-draw smooth></trend></th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <div class="column is-5 buffer">
+        <h3 class="title is-4">Reported Development Aid By Partner</h3>
+        <h6 class="subtitle is-6">US$ Millions, listed in order of magnitude of total aid reported for 2016-18</h6>
+        <table class="table is-bordered is-striped is-narrow is-hoverable">
+          <thead>
+            <tr>
+              <th>Partner</th>
+              <th>2016</th>
+              <th>2017</th>
+              <th>2018</th>
+              <th>Total, 2016-18</th>
+              <th>Trend</th>
+            </tr>
+          </thead>
+          <tbody v-for="row in developmentAid" :key="row.partner">
+            <td>{{ row.partner }}</td>
+            <td>{{ row['2016'] === 0 ? '' : row['2016'] }}</td>
+            <td>{{ row['2017'] === 0 ? '' : row['2017'] }}</td>
+            <td>{{ row['2018'] === 0 ? '' : row['2018']  }}</td>
+            <td>{{ Math.round((row['2016'] + row['2017'] + row['2018']) * 10) / 10 }}</td>
+            <td><trend :data="[row['2016'], row['2017'], row['2018']]" auto-draw smooth></trend></td>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>TOTAL</th>
+              <th>681.2</th>
+              <th>742.1</th>
+              <th>326.0</th>
+              <th>1,749.4</th>
+              <th><trend :data="[681.2, 742.1, 326.0]" auto-draw smooth></trend></th>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </div>
   </div>
@@ -56,6 +127,185 @@
   				['2015', 611, 588],
   				['2016', 681, 563],
   				['2017', 742, 1011],
+  			],
+  			humanitarianAid: [
+  				{
+  					partner: 'United States of America',
+  					'2016': 131.0,
+  					'2017': 388.0,
+  					'2018': 0,
+  					total: 519.0,
+  				},
+  				{
+  					partner: 'United Kingdom',
+  					'2016': 126.4,
+  					'2017': 181.6,
+  					'2018': 52.6,
+  					total: 360.6,
+  				},
+  				{
+  					partner: 'European Commission',
+  					'2016': 53.3,
+  					'2017': 132.7,
+  					'2018': 0,
+  					total: 186.0,
+  				},
+  				{
+  					partner: 'Germany',
+  					'2016': 84.1,
+  					'2017': 24.0,
+  					'2018': 2.8,
+  					total: 110.9,
+  				},
+  				{
+  					partner: 'Denmark',
+  					'2016': 16.6,
+  					'2017': 22.7,
+  					'2018': 19.4,
+  					total: 58.6,
+  				},
+  				{
+  					partner: 'Canada',
+  					'2016': 20.8,
+  					'2017': 30.2,
+  					'2018': 0,
+  					total: 51.0,
+  				},
+  				{
+  					partner: 'Japan',
+  					'2016': 25.4,
+  					'2017': 23.2,
+  					'2018': 0,
+  					total: 48.6,
+  				},
+  				{
+  					partner: 'CERF',
+  					'2016': 12.9,
+  					'2017': 33.0,
+  					'2018': 0,
+  					total: 45.9,
+  				},
+  				{
+  					partner: 'Sweden',
+  					'2016': 22.2,
+  					'2017': 22.4,
+  					'2018': 0,
+  					total: 44.7,
+  				},
+  				{
+  					partner: 'Switzerland',
+  					'2016': 11.5,
+  					'2017': 14.3,
+  					'2018': 14.0,
+  					total: 39.8,
+  				},
+  				{
+  					partner: 'Saudi Arabia',
+  					'2016': 17.4,
+  					'2017': 10.6,
+  					'2018': 0,
+  					total: 28.1,
+  				},
+  				{
+  					partner: 'Australia',
+  					'2016': 5.4,
+  					'2017': 20.6,
+  					'2018': 0,
+  					total: 26.0,
+  				},
+  				{
+  					partner: 'Norway',
+  					'2016': 4.8,
+  					'2017': 17.0,
+  					'2018': 0,
+  					total: 21.8,
+  				},
+  				{
+  					partner: 'Italy',
+  					'2016': 3.9,
+  					'2017': 7.6,
+  					'2018': 4.4,
+  					total: 12.2,
+  				},
+  				{
+  					partner: 'Netherlands',
+  					'2016': 3.9,
+  					'2017': 11.3,
+  					'2018': 0,
+  					total: 15.2,
+  				},
+  				{
+  					partner: 'Ireland',
+  					'2016': 7.3,
+  					'2017': 6.2,
+  					'2018': 0,
+  					total: 13.5,
+  				},
+  				{
+  					partner: 'Finland',
+  					'2016': 6.5,
+  					'2017': 6.6,
+  					'2018': 0,
+  					total: 13.1,
+  				},
+  				{
+  					partner: 'China',
+  					'2016': 2.0,
+  					'2017': 11.0,
+  					'2018': 0,
+  					total: 13.0,
+  				},
+  				{
+  					partner: 'Private, individuals & organisations',
+  					'2016': 0.7,
+  					'2017': 8.0,
+  					'2018': 0,
+  					total: 8.7,
+  				},
+  				{
+  					partner: 'Other',
+  					'2016': 7.0,
+  					'2017': 39.7,
+  					'2018': 0,
+  					total: 46.7,
+  				},
+  			],
+  			developmentAid: [
+  				{
+  					partner: 'European Commission',
+  					'2016': 153.4,
+  					'2017': 155.9,
+  					'2018': 0,
+  				},
+  				{
+  					partner: 'United Kingdom',
+  					'2016': 88.7,
+  					'2017': 76.3,
+  					'2018': 102.0,
+  				},
+  				{ partner: 'United States', '2016': 97.2, '2017': 45.5, '2018': 63.3 },
+  				{ partner: 'Germany', '2016': 48.4, '2017': 114.9, '2018': 0 },
+  				{ partner: 'Sweden', '2016': 41.5, '2017': 41.4, '2018': 50.0 },
+  				{ partner: 'Global Fund', '2016': 56.1, '2017': 46.3, '2018': 13.6 },
+  				{ partner: 'Denmark', '2016': 29.4, '2017': 26.9, '2018': 18.5 },
+  				{ partner: 'Norway', '2016': 34.0, '2017': 35.8, '2018': 0 },
+  				{ partner: 'Italy', '2016': 19.1, '2017': 20.6, '2018': 23.2 },
+  				{
+  					partner: 'African Development Bank',
+  					'2016': 22.1,
+  					'2017': 15.6,
+  					'2018': 20.8,
+  				},
+  				{ partner: 'World Bank', '2016': 0, '2017': 54.8, '2018': 0 },
+  				{ partner: 'Saudi Arabia', '2016': 20.0, '2017': 30.0, '2018': 0 },
+  				{ partner: 'Netherlands', '2016': 17.4, '2017': 15.5, '2018': 10.5 },
+  				{ partner: 'Turkey', '2016': 10.0, '2017': 29.8, '2018': 0 },
+  				{ partner: 'Finland', '2016': 14.2, '2017': 10.4, '2018': 9.1 },
+  				{ partner: 'Switzerland', '2016': 11.2, '2017': 6.6, '2018': 14.0 },
+  				{ partner: 'UN PBF', '2016': 13.4, '2017': 0, '2018': 0 },
+  				{ partner: 'Japan', '2016': 0, '2017': 5.3, '2018': 0 },
+  				{ partner: 'Australia', '2016': 1.9, '2017': 2.7, '2018': 0 },
+  				{ partner: 'Other', '2016': 3.1, '2017': 7.6, '2018': 1.0 },
   			],
   		};
   	},
@@ -171,6 +421,9 @@
   }
   #overview {
   	margin-top: 3%;
+  }
+  #tables {
+  	margin-top: 6%;
   }
 </style>
 
