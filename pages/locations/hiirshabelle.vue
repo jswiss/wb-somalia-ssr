642b68a0ef2a1ea85f0843aeed20f9f00944535c
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-23 10:36:16 +0000

    hiirshabelle done

--- pages/locations/hiirshabelle.vue
+++ pages/locations/hiirshabelle.vue
@@ -1,14 +1,24 @@
 <template lang="html">
   <div id="layouts" class="container is-fluid">
     Hiirshabelle
-    <div id="container"></div>
-    <div id="project-disbursements"></div>
-    <div id="peace-security"></div>
-    <div id="institutions"></div>
-    <div id="economic-growth"></div>
-    <div id="infrastructure"></div>
-    <div id="social-human"></div>
-    <div id="reslience"></div>
+    <div class="columns">
+      <div class="column" id="project-disbursements"></div>
+
+      <div class="column" id="peace-security"></div>
+    </div>
+
+    <div class="columns">
+      <div class="column" id="institutions"></div>
+      <div class="column" id="economic-growth"></div>
+    </div>
+
+    <div class="columns">
+      <div class="column" id="infrastructure"></div>
+      <div class="column" id="social-human"></div>
+    </div>
+
+    <div class="column" id="resilience"></div>
+
   </div>
 </template>
 
@@ -137,7 +147,7 @@ export default {
 
     this.renderClusterChart(this.hiirshabelleSocialHumanDevClusterData, "Hiirshabelle: Social & Human Development", 'social-human');
 
-    this.renderClusterChart(this.hiirshabelleResilienceClusterData, "Hiirshabelle: Resilience", 'reslience');
+    this.renderClusterChart(this.hiirshabelleResilienceClusterData, "Hiirshabelle: Resilience", 'resilience');
 
   }
 };
@@ -152,4 +162,9 @@ export default {
   margin: 0;
   padding: 0;
 }
+
+#project-disbursements, #institutions, #infrastructure, #resilience {
+  height: 350px;
+}
+
 </style>
