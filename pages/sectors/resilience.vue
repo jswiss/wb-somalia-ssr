7a9d6105a5efbf7c80fc8b1e5806253974252e92
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-26 11:05:44 +0000

    resilience done

--- pages/sectors/resilience.vue
+++ pages/sectors/resilience.vue
@@ -194,7 +194,7 @@ export default {
       .fill(this.$store.state.color.blue)
       .labels()
       .enabled(true)
-      .format('${%Value}');
+      .format('${%Value}{groupsSeparator:\\,}');
 
       chart.animation(true);
 
@@ -202,22 +202,14 @@ export default {
 
       chart.yScale().minimum(0);
 
-      chart
-      .tooltip()
-      .displayMode('union')
-      .position('point')
-      .unionFormat(function(){
-        return `Plain: $${
-          this.points[0].value
-        } mln \n Fact: $${this.points[0].value} `;
-      });
+      chart.tooltip().format('${%Value}{groupsSeparator:\\,}');
 
       chart.interactivity().hoverMode('by-x');
 
       chart
       .yAxis()
       .labels()
-      .format('${%Value} mln');
+      .format('${%Value}{groupsSeparator:\\,}');
 
       chart.xAxis(true);
 
@@ -387,7 +379,8 @@ export default {
 
       chart.title("Key Implementers of Reslience Projects");
       chart.container("pie-chart");
-      chart.animation(true)
+      chart.animation(true);
+      chart.tooltip().format('{%x}: ${%Value}{groupsSeparator:\\,}');
 
       chart.draw();
     },
