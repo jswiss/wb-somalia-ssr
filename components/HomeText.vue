47368eebbcc67a5eb9699c80efaedcdddacac156
Joshua Swiss <swissjoshua@gmail.com>
2018-03-09 10:05:32 +0000

    added report to sidebar

--- components/HomeText.vue
+++ components/HomeText.vue
@@ -1,12 +1,19 @@
  <!--home page-->
 <template lang="html">
-  <div class="title">
-    <p class="text" id="top">
-      Aid to Somalia reached a record high in 2017 totaling US$ 1.75 billion. The increase in ODA was primarily driven by the surge in humanitarian support provided in response to the drought. Find out how this financing was delivered in the <a href="https://s3.eu-central-1.amazonaws.com/somalia-aid-flows.so/Aid+Flows+in+Somalia+-+2018FINAL.pdf" target="_blank">latest aid flow report</a>, which provides detailed breakdowns of flows by sector and location.
-    </p>
-    <p class="text">
-      This site provides tools for exploring the underlying data of this report. The 2017 aid mapping exercise was led by the Ministry of Planning, Investment and Economic Development (MoPIED), with the support of the World Bank and United Nations.
-    </p>
+  <div id="side">
+    <div class="title">
+      <p class="text" id="top">
+        Aid to Somalia reached a record high in 2017 totaling US$ 1.75 billion. The increase in ODA was primarily driven by the surge in humanitarian support provided in response to the drought. Find out how this financing was delivered in the <a href="https://s3.eu-central-1.amazonaws.com/somalia-aid-flows.so/Aid+Flows+in+Somalia+-+2018FINAL.pdf" target="_blank">latest aid flow report</a>, which provides detailed breakdowns of flows by sector and location.
+      </p>
+      <p class="text">
+        This site provides tools for exploring the underlying data of this report. The 2017 aid mapping exercise was led by the Ministry of Planning, Investment and Economic Development (MoPIED), with the support of the World Bank and United Nations.
+      </p>
+    </div>
+    <div id="report">
+      <h1 class="title is-4">Download the Latest Report</h1>
+      <br>
+      <a href="https://s3.eu-central-1.amazonaws.com/somalia-aid-flows.so/Aid+Flows+in+Somalia+-+2018FINAL.pdf"><img src="https://s3.eu-central-1.amazonaws.com/somalia-aid-flows.so/2018AidFlowsCover.png"/></a>
+    </div>
   </div>
 </template>
 
@@ -29,5 +36,9 @@
   	line-height: 1.5;
   	margin: 3%;
   	font-weight: 400;
+  }
+  #report {
+  	margin-top: 2%;
+  	text-align: center;
   }
 </style>
