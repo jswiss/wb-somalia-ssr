4429fe4016c8ee26869a15acbba1076ff66ad123
Joshua Swiss <swissjoshua@gmail.com>
2018-03-12 11:16:47 +0000

    location table up on location page

--- pages/locations/index.vue
+++ pages/locations/index.vue
@@ -29,7 +29,7 @@
         </div>
         <div class="column is-1 is-mobile"></div>
       </div>
-      <div class="columns is-narrow">
+      <div class="columns">
         <div class="column is-10 is-mobile">
           <tree-map-2016 v-if="tree === 2016"></tree-map-2016>
           <tree-map-2018 v-if="tree === 2018"></tree-map-2018>
@@ -37,12 +37,53 @@
         </div>
         <div class="column is-1 is-mobile"></div>
       </div>
+      <br>
+      <br>
+      <div class="columns">
+        <!-- <div class="column is-1"></div> -->
+        <div class="column is-10 is-mobile">
+          <h1 class="title">Number of Projects and Total Project Disbursements by Year and Location</h1>
+          <v-client-table
+              :data="locationTable"
+              :columns="columns"
+              :options="options"
+            >
+              <template slot="2016 Total Disbursements" slot-scope="props">
+                <div>
+                  ${{ props.row['2016 Total Disbursements'] | currency }}
+                </div>
+              </template>
+              <template slot="2017 Total Disbursements" slot-scope="props">
+                <div>
+                  ${{ props.row['2017 Total Disbursements'] | currency }}
+                </div>
+              </template>
+              <template slot="2018 Total Disbursements" slot-scope="props">
+                <div>
+                  ${{ props.row['2018 Total Disbursements'] | currency }}
+                </div>
+              </template>
+              <template slot="Number of Projects Trend" slot-scope="props">
+                <div>
+                  <trend :data="[props.row['2016 Count of Projects'], props.row['2017 Count of Projects'], props.row['2018 Count of Projects'] ]" auto-draw smooth></trend>
+                </div>
+              </template>
+              <template slot="Total Disbursements by Location Trend" slot-scope="props">
+                <div>
+                  <trend :data="[props.row['2016 Total Disbursements'], props.row['2017 Total Disbursements'], props.row['2018 Total Disbursements'] ]" auto-draw smooth></trend>
+                </div>
+              </template>
+            </v-client-table>
+          </div>
+        <!-- <div class="column is-1"></div> -->
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import axios from 'axios';
+  import { mapState } from 'vuex';
   import LocationsMenu from '~/components/LocationsMenu';
   import TreeMap2016 from '~/components/TreeMap2016';
   import TreeMap2017 from '~/components/TreeMap2017';
@@ -55,6 +96,22 @@
   		return {
   			tree2017: true,
   			tree: '',
+  			columns: [
+  				'Location',
+  				'2016 Count of Projects',
+  				'2017 Count of Projects',
+  				'2018 Count of Projects',
+  				'2016 Total Disbursements',
+  				'2017 Total Disbursements',
+  				'2018 Total Disbursements',
+  				'Number of Projects Trend',
+  				'Total Disbursements by Location Trend',
+  			],
+  			options: {
+  				saveState: true,
+  				highlightMatches: true,
+  				storage: 'local',
+  			},
   		};
   	},
   	components: {
@@ -62,24 +119,11 @@
   		TreeMap2017,
   		TreeMap2018,
   	},
-  	// async asyncData({ query, error, store }) {
-  	// 	if (store.state.treeMap2016.length !== 0) return;
-
-  	// 	let [tree2016, tree2017, tree2018] = await Promise.all([
-  	// 		axios.get('https://api.80pco.com/2016'),
-  	// 		axios.get('https://api.80pco.com/2017'),
-  	// 		axios.get('https://api.80pco.com/2018'),
-  	// 	]);
-  	// 	store.commit('SET_TREE_2016', tree2016.data);
-  	// 	store.commit('SET_TREE_2017', tree2017.data);
-  	// 	store.commit('SET_TREE_2018', tree2018.data);
-
-  	// 	return {
-  	// 		tree2016: tree2016.data,
-  	// 		tree2017: tree2017.data,
-  	// 		tree2018: tree2018.data,
-  	// 	};
-  	// },
+  	computed: {
+  		...mapState({
+  			locationTable: state => state.locationTable,
+  		}),
+  	},
   };
 </script>
 
